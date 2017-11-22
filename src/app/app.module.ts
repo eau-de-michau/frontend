619b2785322979d9cf232d2e7f49881f04d6d0d9
Michał Gościewski <michal.gosciewski@gmail.com>
2017-11-22 09:20:37 +0100

    Prognosisfront

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ClientComponent } from './client/client.component';
+import { ModeratorComponent } from './moderator/moderator.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClientComponent,
+    ModeratorComponent
   ],
   imports: [
     BrowserModule,
